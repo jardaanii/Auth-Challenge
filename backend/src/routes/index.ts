e0b7b6a49709d9aa7a4433783011b6bdaac549d0
Akash <kiraacodes@gmail.com>
2024-09-04 19:23:11 +0530

    Finished the Signup route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,4 +1,8 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import bcrypt from "bcryptjs";
+import { sign } from "hono/jwt";
 
 type Bindings = {
   DATABASE_URL: string;
@@ -9,10 +13,49 @@ type Bindings = {
 export const apiRoutes = new Hono<{ Bindings: Bindings }>();
 
 apiRoutes.post("/v1/signup", async (c) => {
-  return c.text("Hello form signup");
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const hashedPassword = await bcrypt.hash(body.password, c.env.SALT);
+  try {
+    const response = await prisma.user.create({
+      data: {
+        email: body.email,
+        name: body.name,
+        isCreator: body.isCreator,
+        password: hashedPassword,
+      },
+    });
+
+    const token = await sign(
+      {
+        id: response.id,
+        email: body.email,
+        name: body.name,
+      },
+      c.env.JWT_SECRET
+    );
+    return c.json({
+      status: 200,
+      message: "Successfully created the user",
+      data: token,
+    });
+  } catch (error) {
+    c.status(411);
+    return c.json({
+      message: "error while creating a user",
+    });
+  }
 });
 
 apiRoutes.post("/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  //    const token = await sign({ id: id, email: email }, c.env.JWT_SECRET);
   return c.text("Hello form signin");
 });
 
