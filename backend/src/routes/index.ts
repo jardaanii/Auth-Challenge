36e81693a8d85af9402034f734a3157251756dd2
Akash <kiraacodes@gmail.com>
2024-09-04 19:38:53 +0530

    fixed the try catch

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -56,39 +56,46 @@ apiRoutes.post("/v1/signin", validateSignupInput, async (c) => {
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
-  const response = await prisma.user.findFirst({
-    where: {
-      email: body.email,
-    },
-    select: {
-      id: true,
-      password: true,
-    },
-  });
+  try {
+    const response = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
 
-  if (!response) {
-    return c.text("Not able to find the user");
+    if (!response) {
+      return c.text("Not able to find the user");
+    }
+    const isPasswordValid = await bcrypt.compare(
+      body.password,
+      response.password
+    );
+    if (!isPasswordValid) {
+      c.status(401);
+      return c.json({ message: "Invalid credentials" });
+    }
+    const token = await sign(
+      {
+        id: response.id,
+        email: body.email,
+      },
+      c.env.JWT_SECRET
+    );
+    return c.json({
+      status: 200,
+      message: "Successfully logged in the user",
+      data: token,
+    });
+  } catch (error) {
+    c.status(411);
+    return c.json({
+      message: "error while creating a user",
+    });
   }
-  const isPasswordValid = await bcrypt.compare(
-    body.password,
-    response.password
-  );
-  if (!isPasswordValid) {
-    c.status(401);
-    return c.json({ message: "Invalid credentials" });
-  }
-  const token = await sign(
-    {
-      id: response.id,
-      email: body.email,
-    },
-    c.env.JWT_SECRET
-  );
-  return c.json({
-    status: 200,
-    message: "Successfully logged in the user",
-    data: token,
-  });
 });
 
 export default apiRoutes;
